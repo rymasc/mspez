45433c51ad2b1f48241d477507be1c08334ad028
Ryan Scott <ryan.matt.scott@gmail.com>
2018-08-31 16:05:03 -0400

    install mspdebug

--- setup.sh
+++ setup.sh
@@ -12,12 +12,32 @@
 #   pip install requests
 #fi
 
+echo 'Downloading installation files ... '
 python scrape.py
 
+echo 'installing mspgcc ...'
 mkdir -p /home/$USER/ti/msp430-gcc 
 sudo tar xvjf mspgcc.tar.bz2 -C /home/$USER/ti/msp430-gcc --strip-components 1
 sudo unzip support-files.zip 
 sudo cp -a msp430-gcc-support-files/. /home/$USER/ti/msp430-gcc
+
+echo 'mspgcc ..installed'
+
+echo 'installing mspdebug'
+sudo mkdir -p /home/$USER/mspdebug
+sudo cp mspdebug.zip /home/$USER/mspdebug
+cd /home/$USER/mspdebug
+sudo unzip mspdebug.zip
+sudo mv libmsp430.so libmsp430_32.so
+sudo mv libmsp430_64.so libmsp430.so
+sudo git clone https://github.com/dlbeer.com/mspdebug
+cd mspdebug
+sudo make
+sudo make install
+export LD_LIBRARY_PATH=/home/$USER/mspdebug
+echo ' ok... mspdebug should be installed and it should all work now.'
+
+
 
 sudo rm -rf support-files.zip
 sudo rm -rf msp430-gcc-support-files
