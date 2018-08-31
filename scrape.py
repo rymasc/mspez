ab68d4716035a6aad995a8bbe64382518d5e934f
Ryan Scott <ryan.matt.scott@gmail.com>
2018-08-31 14:32:52 -0400

    fix wget

--- scrape.py
+++ scrape.py
@@ -11,7 +11,7 @@ page = requests.get(ti_url)
 for link in BeautifulSoup(page.text, parse_only=SoupStrainer('a'), features='html.parser'):
     if link.has_attr('href') and 'linux64' in link['href']:
         print link['href']
-	os.system('wget -O mspgcc.tar.bz2' + ti_gcc_url + link['href'])
+	os.system('wget ' + ti_gcc_url + link['href']+ ' -O mspgcc.tar.bz2')
 
 
 ti_debug_url = "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/latest/"
--- setup.sh
+++ setup.sh
@@ -2,6 +2,12 @@ sudo apt-get install build-essential
 sudo apt-get install libusb-dev libreadline-dev
 sudo apt-get install python -y
 sudo apt-get install python-bs4
-sudo apt-get install python-setuptools
 sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python-setuptools
+#if 16.04
 sudo easy_install requests
+#if 18.04
+pip install requests
+
+
