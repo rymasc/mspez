1a95b3e5f804d5b2e82168804d6655aa6e2a16aa
Ryan Scott <ryan.matt.scott@gmail.com>
2018-08-31 14:19:40 -0400

    Add setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
+sudo apt-get install build-essential
+sudo apt-get install libusb-dev libreadline-dev
 sudo apt-get install python -y
 sudo apt-get install python-bs4
 sudo apt-get install python-setuptools
+sudo apt-get update
 sudo easy_install requests
