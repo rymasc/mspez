ca225a87b1a27e124fba3673368088fab64ac3c1
Ryan Scott <ryan.matt.scott@gmail.com>
2018-08-31 16:04:33 -0400

    download msp debug stack

--- scrape.py
+++ scrape.py
@@ -8,23 +8,25 @@ ti_url = ti_gcc_url+"index_FDS.html"
 headers ={'User-Agent':'Mozilla/5.0'}
 page = requests.get(ti_url)
 
+#extract mspgcc toolchain
 for link in BeautifulSoup(page.text, parse_only=SoupStrainer('a'), features='html.parser'):
     if link.has_attr('href') and 'linux64' in link['href']:
         print link['href']
 	os.system('wget ' + ti_gcc_url + link['href']+ ' -O mspgcc.tar.bz2')
 
+#extract mspgcc include
 for link in BeautifulSoup(page.text, parse_only=SoupStrainer('a'), features='html.parser'):
     if link.has_attr('href') and 'support-files' in link['href']:
         print link['href']
 	os.system('wget ' + ti_gcc_url + link['href']+ ' -O support-files.zip')
 
 
-ti_debug_url = "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPGCC/latest/"
+ti_debug_url = "http://software-dl.ti.com/msp430/msp430_public_sw/mcu/msp430/MSPDS/latest/"
 ti_url = ti_gcc_url+"index_FDS.html"
 headers ={'User-Agent':'Mozilla/5.0'}
 page = requests.get(ti_url)
 
-
-#for link in BeautifulSoup(page.text, parse_only=SoupStrainer('a'), features='html.parser'):
-#    if link.has_attr('href') and 'MSP430 DLL' in link['href']:
-#        print link['href']
+#extract MSPDS DL
+for link in BeautifulSoup(page.text, parse_only=SoupStrainer('a'), features='html.parser'):
+    if link.has_attr('href') and 'MSP430_DLL' in link['href']:
+        os.system('wget ' + ti_debug_url + link['href'] + ' -O mspdebug.zip')
