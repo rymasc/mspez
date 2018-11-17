f13396477a825b2361388f1d0d378fc4a744c59c
Ryan Scott <ryan.matt.scott@gmail.com>
2018-11-17 11:26:04 -0500

    Fix mspdebug link

--- setup.sh
+++ setup.sh
@@ -1,16 +1,16 @@
-#sudo apt-get install build-essential
-#sudo apt-get install libusb-dev libreadline-dev
-#sudo apt-get install python -y
-#sudo apt-get install python-bs4
-#sudo apt-get update
-#sudo apt-get upgrade
-#sudo apt-get install python-setuptools
-#v="$(lsb_release -r | awk '{print $2}')"
-#if [ "${v}" = 16.04 ]; then
-#   sudo easy_install requests
-#else
-#   pip install requests
-#fi
+sudo apt-get install build-essential
+sudo apt-get install libusb-dev libreadline-dev
+sudo apt-get install python -y
+sudo apt-get install python-bs4
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install python-setuptools
+v="$(lsb_release -r | awk '{print $2}')"
+if [ "${v}" = 16.04 ]; then
+   sudo easy_install requests
+else
+   pip install requests
+fi
 
 echo 'Downloading installation files ... '
 python scrape.py
@@ -30,7 +30,7 @@ cd /home/$USER/mspdebug
 sudo unzip mspdebug.zip
 sudo mv libmsp430.so libmsp430_32.so
 sudo mv libmsp430_64.so libmsp430.so
-sudo git clone https://github.com/dlbeer.com/mspdebug
+sudo git clone https://github.com/dlbeer/mspdebug
 cd mspdebug
 sudo make
 sudo make install
